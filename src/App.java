b3f9337e2d773a8a1e2ed40ec53ac833318a1beb
DungSV <1000000001@student.humg.edu.vn>
2025-02-25 14:44:48 +0700

    0001

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Mr Dung!");
+        System.out.println("Hello, Mr Dung!!!!2025");
     }
 }
